e412713870a371ee2b6e340800c11deb04dc7f78
unknown <benipal@uw.edu>
2020-12-29 12:18:41 -0800

    Add comments for each algorithm

--- algorithms/algorithms.go
+++ algorithms/algorithms.go
@@ -2,6 +2,12 @@ package algorithms
 
 import "fmt"
 
+/*
+InsertionSort - O(N^2)
+works similar to the way you sort playing cards in your hands.
+The array is virtually split into a sorted and an unsorted part.
+Values from the unsorted part are picked and placed at the correct position in the sorted part.
+*/
 func InsertionSort(data []int) {
 	for i := 1; i < len(data); i++ {
 		j := i - 1
@@ -13,11 +19,15 @@ func InsertionSort(data []int) {
 			j--
 		}
 		fmt.Printf("End of iteration %d and j: %d \n", i, j)
-		fmt.Printf("%v \n", data)
+		fmt.Printf("Insertion Sort: %v", data)
 		data[j+1] = temp
 	}
 }
 
+/*
+BubbleSort - O(N^2)
+simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+*/
 func BubbleSort(data []int) {
 	for i := 0; i < len(data); i++ {
 		for j := 1; j < len(data)-1; j++ {
@@ -26,9 +36,15 @@ func BubbleSort(data []int) {
 			}
 		}
 	}
-	fmt.Printf("%v", data)
+	fmt.Printf("Bubble Sort: %v", data)
 }
 
+/*
+SelectionSort - O(N^2)
+sorts an array by repeatedly finding the minimum element (considering ascending order)
+from unsorted part and putting it at the beginning.
+The algorithm maintains two subarrays in a given array.
+*/
 func SelectionSort(data []int) {
 	length := len(data)
 	for i := 0; i < length; i++ {
